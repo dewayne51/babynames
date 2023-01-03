5c77b817875aaea3c2908bfb308242cfd9e9a78c
Dewayne Sewell <dewaynesewell1@gmail.com>
2023-01-03 16:00:19 +0000

    babynames

---
+++ src/childList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import jsonData from "./babyNamesData.json";
+import "./childList.css";
+
+const ChildList = () => {
+  return (
+     <div>
+    <p>Baby Name picker.</p>
+    <div className="container-fluid">
+        {
+            jsonData.map((baby, index) => (
+            <button key={index}
+                    className={
+                    baby.sex === "f"
+                        ? "p-3 mb-2 baby-girl text-white"
+                        : "p-3 mb-2 baby-boy text-white"}>
+                    {baby.name}
+            </button>
+            ))
+        }
+    </div>
+    </div>
+  );
+};
+
+export default ChildList;
